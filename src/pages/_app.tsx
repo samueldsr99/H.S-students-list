c169171f510032c212d9ea8e307f93a6c1a7ddee
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-09-24 16:13:56 -0400

    feat: remove `react-query`
    make the site static instead

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,7 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
-
-import queryClient from "@/lib/config/queryclient.config";
 
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- src/react-query/hooks/useGetCourse.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-
-import studentListService from "@/lib/services";
-
-import * as querykeys from "../querykeys";
-
-const useGetCourse = () => useQuery(querykeys.getCourse(), studentListService.courses.get);
-
-export default useGetCourse;
