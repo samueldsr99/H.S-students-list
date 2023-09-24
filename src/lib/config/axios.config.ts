38f277fbb5bde1bd39b978635258e49a7b979964
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-09-23 22:44:26 -0400

    feat: setup `axios` & `react-query`

---
+++ src/lib/config/axios.config.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const httpService = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+export default httpService;
